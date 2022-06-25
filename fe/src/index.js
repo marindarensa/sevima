26caec06c695562135b0a84278b02c9db1c5fff2
marindarensa <62869131+marindarensa@users.noreply.github.com>
2022-06-25 19:50:39 +0700

    update

---
+++ fe/src/index.js
@@ -0,0 +1,16 @@
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
