c0f8766a309befe87e74cf01b6b7bf921a01cfa0
marindarensa <62869131+marindarensa@users.noreply.github.com>
2022-06-25 20:40:13 +0700

    update

---
+++ fe/src/Pages/Dashboard.js
@@ -0,0 +1,356 @@
+import React from "react";
+import axios from "axios";
+import { base_url } from "../Config.js";
+import { Link } from "react-router-dom";
+
+// components
+import Tabs from "../Components/Tabs";
+import RIWAYAT_PEMBAYARAN from "../Components/Table_Riwayat_Pembayaran";
+import Kelas from "../Components/Table_Kelas";
+import Petugas from "../Components/Table_Petugas";
+import Siswa from "../Components/Table_Siswa";
+import Spp from "../Components/Table_Spp";
+import Laporan from "../Components/Table_laporan";
+import Tunggakan from "../Components/Table_Tunggakan";
+
+class Dashboard extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            token: "",
+            userData: "",
+            data_pembayaran: [],
+            data_petugas: [],
+            data_siswa: [],
+            data_kelas: [],
+            message: "",
+            found: "",
+        };
+        // dapetin token dari localstorage
+        if (localStorage.getItem("token")) {
+            this.state.token = localStorage.getItem("token");
+        } else {
+            // token ga ada, redirect ke laman login
+            window.location = "/login";
+        }
+    }
+
+    getUser = () => {
+        let userdata = JSON.parse(localStorage.getItem("user"));
+        this.setState({
+            userData: userdata,
+        });
+    };
+
+    getDataPembayaran = () => {
+        let url = base_url + "/pembayaran";
+
+        axios
+            .get(url, {
+                headers: {
+                    Authorization: "Bearer " + this.state.token,
+                },
+            })
+            .then((response) => {
+                this.setState({
+                    found: response.data.found,
+                });
+                if (this.state.found) {
+                    let data_pembayaran = JSON.parse(
+                        JSON.stringify(response.data.pembayaran)
+                    );
+
+                    this.setState({
+                        data_pembayaran: data_pembayaran,
+                    });
+                } else {
+                    this.setState({
+                        message: response.data.message,
+                    });
+                }
+            })
+            .catch((error) => console.log(error));
+    };
+
+    getDataPetugas = () => {
+        let url = base_url + "/petugas";
+
+        axios
+            .get(url, {
+                headers: {
+                    Authorization: "Bearer " + this.state.token,
+                },
+            })
+            .then((response) => {
+                let data_petugas = JSON.parse(JSON.stringify(response.data.petugas));
+                this.setState({
+                    data_petugas: data_petugas,
+                });
+            })
+            .catch((error) => console.log(error));
+    };
+
+    getDataSiswa = () => {
+        let url = base_url + "/siswa";
+
+        axios
+            .get(url, {
+                headers: {
+                    Authorization: "Bearer " + this.state.token,
+                },
+            })
+            .then((response) => {
+                let data_siswa = JSON.parse(JSON.stringify(response.data.siswa));
+                this.setState({
+                    data_siswa: data_siswa,
+                });
+            })
+            .catch((error) => console.log(error));
+    };
+
+    getDataKelas = () => {
+        let url = base_url + "/kelas";
+
+        axios
+            .get(url, {
+                headers: {
+                    Authorization: "Bearer " + this.state.token,
+                },
+            })
+            .then((response) => {
+                let data_kelas = JSON.parse(JSON.stringify(response.data.kelas));
+                this.setState({
+                    data_kelas: data_kelas,
+                });
+            })
+            .catch((error) => console.log(error));
+    };
+
+    Logout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        window.location = "/login";
+    };
+
+    timeCheck = () => {
+        const today = new Date();
+        const date = today.getFullYear() + "-" + today.getMonth();
+        // let tomorrow = new Date();
+        // tomorrow.setDate(today.getDate() + 1);
+        console.log(date);
+        // console.log(tomorrow);
+    };
+
+    componentDidMount() {
+        this.getUser();
+        this.getDataPembayaran();
+        this.getDataPetugas();
+        this.getDataSiswa();
+        this.getDataKelas();
+        this.timeCheck();
+    }
+    render() {
+        return (
+            <div>
+                {/* Page title starts */}
+                <div className="mt-10 lg:mt-9 container px-6 mx-auto flex flex-col lg:flex-row items-center justify-between pb-4 border-b border-gray-300">
+                    <div className="mx-auto">
+                        <h4 className="text-2xl font-bold leading-tight text-gray-800 text-center">
+                            {this.state.userData.nama_petugas}
+                        </h4>
+                        <ul className="flex flex-col md:flex-row items-start md:items-center text-gray-600 text-sm mt-3">
+                            <li className="flex items-center mr-3 mt-3 md:mt-0">
+                                <span className="mr-2">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="17"
+                                        height="17"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="#000000"
+                                        stroke-width="1.5"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="bevel"
+                                    >
+                                        <rect
+                                            x="4"
+                                            y="4"
+                                            width="16"
+                                            height="16"
+                                            rx="2"
+                                            ry="2"
+                                        ></rect>
+                                        <rect x="9" y="9" width="6" height="6"></rect>
+                                        <line x1="9" y1="1" x2="9" y2="4"></line>
+                                        <line x1="15" y1="1" x2="15" y2="4"></line>
+                                        <line x1="9" y1="20" x2="9" y2="23"></line>
+                                        <line x1="15" y1="20" x2="15" y2="23"></line>
+                                        <line x1="20" y1="9" x2="23" y2="9"></line>
+                                        <line x1="20" y1="14" x2="23" y2="14"></line>
+                                        <line x1="1" y1="9" x2="4" y2="9"></line>
+                                        <line x1="1" y1="14" x2="4" y2="14"></line>
+                                    </svg>
+                                </span>
+                                <span>Username: {this.state.userData.username}</span>
+                            </li>
+                            <li className="flex items-center mr-3 mt-3 md:mt-0">
+                                <span className="mr-2">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="17"
+                                        height="17"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="#000000"
+                                        stroke-width="1.5"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="bevel"
+                                    >
+                                        <circle cx="12" cy="12" r="10" />
+                                        <path d="M16.2 7.8l-2 6.3-6.4 2.1 2-6.3z" />
+                                    </svg>
+                                </span>
+                                <span>Role: {this.state.userData.level}</span>
+                            </li>
+                            <li className="flex items-center mt-3 md:mt-0">
+                                <span className="mr-2">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="17"
+                                        height="17"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="#000000"
+                                        stroke-width="1.5"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="bevel"
+                                    >
+                                        <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                        <circle cx="8.5" cy="7" r="4"></circle>
+                                        <polyline points="17 11 19 13 23 9"></polyline>
+                                    </svg>
+                                </span>
+                                <span>ID: {this.state.userData.id_petugas}</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="mt-6">
+                        <Link to="/setting">
+                            <button className="mx-2 my-2 bg-white transition duration-150 ease-in-out focus:outline-none hover:bg-gray-100 rounded text-indigo-700 px-6 py-2 text-sm">
+                                Setting
+                            </button>
+                        </Link>
+                        <button
+                            onClick={() => this.Logout()}
+                            className="transition duration-150 ease-in-out hover:bg-indigo-600 focus:outline-none border bg-indigo-700 rounded text-white px-8 py-2 text-sm"
+                        >
+                            Logout
+                        </button>
+                    </div>
+                </div>
+                <div className="w-full bg-gray-200 dark:bg-gray-900 py-10">
+                    <div className="container mx-auto px-6 flex items-start justify-center">
+                        <div className="w-full">
+                            {/* Card code block start */}
+                            <div className="flex flex-col lg:flex-row mx-auto w-full bg-white dark:bg-gray-800 shadow rounded">
+                                <div className="w-full p-6 border-t border-b lg:border-t-0 lg:border-b-0 sm:border-l sm:border-r border-gray-300">
+                                    <div className="flex flex-col lg:flex-row items-start lg:items-center justify-between">
+                                        <div className="mt-3 text-xl text-center w-full bg-indigo-100 text-indigo-700 dark:text-indigo-600 rounded font-medium p-3 lg:mr-3">
+                                            Jumlah Transaksi: {this.state.data_pembayaran.length}
+                                        </div>
+                                        <div className="mt-3 text-xl text-center w-full bg-indigo-100 text-indigo-700 dark:text-indigo-600 rounded font-medium p-3 lg:mr-3">
+                                            Jumlah Siswa: {this.state.data_siswa.length}
+                                        </div>
+                                        <div className="mt-3 text-xl text-center w-full bg-indigo-100 text-indigo-700 dark:text-indigo-600 rounded font-medium p-3 lg:mr-3">
+                                            Jumlah Petugas: {this.state.data_petugas.length}
+                                        </div>
+                                        <div className="mt-3 text-xl text-center w-full bg-indigo-100 text-indigo-700 dark:text-indigo-600 rounded font-medium p-3">
+                                            Jumlah Kelas: {this.state.data_kelas.length}
+                                        </div>
+                                    </div>
+                                    <div className="mt-6 mb-8 w-full bg-gray-100 dark:bg-gray-700 rounded p-4 relative break-words">
+                                        <h4 className="text-2xl font-bold leading-tight text-gray-800 mb-4">
+                                            Data lengkap {this.state.userData.level}:{" "}
+                                        </h4>
+                                        <ul>
+                                            <li className="text-md text-gray-600 dark:text-gray-400 font-normal tracking-normal my-2">
+                                                - ID {this.state.userData.level}:{" "}
+                                                {this.state.userData.id_petugas}
+                                            </li>
+                                            <li className="text-md text-gray-600 dark:text-gray-400 font-normal tracking-normal my-2">
+                                                - Username: {this.state.userData.username}
+                                            </li>
+                                            <li className="text-md text-gray-600 dark:text-gray-400 font-normal tracking-normal my-2">
+                                                - Nama: {this.state.userData.nama_petugas}
+                                            </li>
+                                            <li className="text-md text-gray-600 dark:text-gray-400 font-normal tracking-normal my-2">
+                                                - Role: {this.state.userData.level}
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-full bg-gray-200 dark:bg-gray-900 pb-10">
+                    <div className="container mx-auto px-6 flex items-start justify-center">
+                        <div className="w-full">
+                            {/* Tabs 1 */}
+                            <h4 className="text-2xl font-bold leading-tight text-gray-800">
+                                CRUD Tabs
+                            </h4>
+                            <Tabs
+                                color="indigo"
+                                title1="Petugas"
+                                tab1={<Petugas />}
+                                title2="Siswa"
+                                tab2={<Siswa />}
+                                title3="Kelas"
+                                tab3={<Kelas />}
+                            />
+
+                            {/* Tabs 2 */}
+                            <h4 className="text-2xl font-bold leading-tight text-gray-800">
+                                Transaksi Tabs
+                            </h4>
+                            <Tabs
+                                color="indigo"
+                                title1="Riwayat Pembayaran"
+                                tab1={<RIWAYAT_PEMBAYARAN />}
+                                title2="Tunggakan"
+                                tab2={<Tunggakan />}
+                                title3="SPP"
+                                tab3={<Spp />}
+                            />
+
+                            <div className="text-center mt-4">
+                                <button
+                                    type="button"
+                                    className="text-gray-800 text-sm"
+                                    onClick={() => this.Logout()}
+                                >
+                                    Kembali ke halaman login
+                                </button>
+                                <p className="text-gray-500 text-sm">
+                                    Hello World! |{" "}
+                                    <a
+                                        href="https://github.com/wisnusat"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        It's me Wisnu
+                                    </a>
+                                </p>
+                                <p className="text-gray-500 text-sm">&copy;2021</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
---
+++ fe/src/Pages/Invoice.js
@@ -0,0 +1,211 @@
+import React, { Component } from "react";
+
+import $ from "jquery";
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
+
+export default class Invoice extends Component {
+    constructor() {
+        super();
+        this.state = {
+            id_pembayaran: "",
+            nisn: "",
+            tgl_bayar: "",
+            bulan_dibayar: "",
+            tahun_dibayar: "",
+            id_spp: "",
+            jumlah_bayar: "",
+            id_petugas: "",
+            nama_petugas: "",
+            nama_siswa: "",
+            kelas: "",
+            nominal: "",
+        };
+    }
+    getData = () => {
+        this.setState({
+            id_pembayaran: localStorage.getItem("id_pembayaran"),
+            nisn: localStorage.getItem("nisn"),
+            tgl_bayar: localStorage.getItem("tgl_bayar"),
+            bulan_dibayar: localStorage.getItem("bulan_dibayar"),
+            tahun_dibayar: localStorage.getItem("tahun_dibayar"),
+            id_spp: localStorage.getItem("id_spp"),
+            jumlah_bayar: localStorage.getItem("jumlah_bayar"),
+            id_petugas: localStorage.getItem("id_petugas"),
+            nama_petugas: localStorage.getItem("nama_petugas"),
+            nama_siswa: localStorage.getItem("nama_siswa"),
+            kelas: localStorage.getItem("kelas"),
+            nominal: localStorage.getItem("nominal"),
+        });
+    };
+
+    getPDF = (ev) => {
+        ev.preventDefault();
+        var HTML_Width = $("article").width();
+        var HTML_Height = $("article").height();
+        var top_left_margin = 15;
+        var PDF_Width = HTML_Width + top_left_margin * 2;
+        var PDF_Height = PDF_Width * 1 + top_left_margin * 2;
+        var canvas_image_width = HTML_Width;
+        var canvas_image_height = HTML_Height;
+        var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+        html2canvas($("article")[0], { allowTaint: true }).then(function (canvas) {
+            canvas.getContext("2d");
+            var imgData = canvas.toDataURL("image/jpeg", 1.0);
+            var pdf = new jsPDF("p", "pt", [PDF_Width, PDF_Height]);
+            pdf.addImage(
+                imgData,
+                "JPG",
+                top_left_margin,
+                top_left_margin,
+                canvas_image_width,
+                canvas_image_height
+            );
+            for (var i = 1; i <= totalPDFPages; i++) {
+                pdf.addPage(PDF_Width, PDF_Height);
+                pdf.addImage(
+                    imgData,
+                    "JPG",
+                    top_left_margin,
+                    -(PDF_Height * i) + top_left_margin * 4,
+                    canvas_image_width,
+                    canvas_image_height
+                );
+            }
+            pdf.save("Invoice.pdf");
+        });
+    };
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    render() {
+        return (
+            <>
+                <article>
+                    <div className="pt-5 sm:pt-0 bg-white h-full">
+                        <div class="max-w-7xl mx-auto">
+                            <div class="p-10 font-mono">
+                                <div class="grid grid-cols-1 gap-5 sm:grid-cols-2">
+                                    <div class="flex flex-col">
+                                        <h2 class="text-4xl sm:py-5">{this.state.nama_siswa}</h2>
+                                        <span class="text-gray-500">{this.state.nisn}</span>
+                                    </div>
+
+                                    <div class="flex flex-col sm:justify-end sm:items-end">
+                                        <h2 class="text-4xl text-gray-500 sm:py-5">
+                                            Payment Receipt
+                                        </h2>
+
+                                        <dl class="max-w-1/2">
+                                            <div class="grid grid-cols-2 items-end">
+                                                <dt class="text-gray-500 ">Payment ID</dt>
+                                                <dd class="text-right">{this.state.id_pembayaran}</dd>
+                                            </div>
+                                            <div class="grid grid-cols-2">
+                                                <dt class="text-gray-500">Date paid</dt>
+                                                <dd class="text-right">{this.state.tgl_bayar}</dd>
+                                            </div>
+                                        </dl>
+                                    </div>
+
+                                    <div class="md:col-span-2">
+                                        <h4 class="text-lg">Authorized by</h4>
+                                        <p class="text-gray-500">{this.state.nama_petugas}</p>
+                                        <p class="text-gray-500">{this.state.id_petugas}</p>
+                                    </div>
+                                </div>
+
+                                <div class="flex flex-col">
+                                    <h1 class="py-10 sm:py-20 text-3xl">
+                                        Rp{this.state.jumlah_bayar} paid on {this.state.tgl_bayar}
+                                    </h1>
+
+                                    <div class="overflow-scroll">
+                                        <table class="min-w-full">
+                                            <thead class="text-bold">
+                                                <tr>
+                                                    <th scope="col" class="text-left w-2/5 sm:px-8 py-4">
+                                                        Description
+                                                    </th>
+                                                    <th scope="col" class="text-left w-1/5 sm:px-8 py-4">
+                                                        Month & Year
+                                                    </th>
+                                                    <th scope="col" class="text-left w-1/5 sm:px-8 py-4">
+                                                        Amount
+                                                    </th>
+                                                    <th scope="col" class="text-right w-1/5 sm:px-8 py-4">
+                                                        Total Paid
+                                                    </th>
+                                                </tr>
+                                            </thead>
+
+                                            <tbody class="divide-y divide-gray-200">
+                                                <tr class="text-left bg-gray-50">
+                                                    <td class="sm:px-8 py-4">
+                                                        Spp {this.state.kelas} Payment
+                                                    </td>
+                                                    <td class="sm:px-8 py-4">
+                                                        {this.state.bulan_dibayar} /{" "}
+                                                        {this.state.tahun_dibayar}
+                                                    </td>
+                                                    <td class="sm:px-8 py-4">{this.state.nominal}</td>
+                                                    <td class="sm:px-8 py-4 text-right">
+                                                        {this.state.jumlah_bayar}
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td colspan="2" class="">
+                                                        &nbsp;
+                                                    </td>
+                                                    <td class="sm:px-8 py-4">Change</td>
+                                                    <td class="sm:px-8 py-4 text-right">
+                                                        {this.state.jumlah_bayar > this.state.nominal
+                                                            ? this.state.jumlah_bayar - this.state.nominal
+                                                            : 0}
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <div className="px-6 py-2">
+                    <button
+                        className="bg-green-500 text-white active:bg-indigo-600
+font-bold uppercase text-sm px-6 py-3 rounded shadow
+hover:shadow-lg outline-none focus:outline-none mr-1 mb-1
+ease-linear transition-all duration-150"
+                        type="button"
+                    >
+                        <a
+                            href="#generate-invoice-pdf"
+                            target="_blank"
+                            class="inline-flex items-center text-sm font-medium"
+                            onClick={(ev) => this.getPDF(ev)}
+                            style={{ textDecoration: "none", color: "white" }}
+                        >
+                            {" "}
+                            Download PDF{" "}
+                            <svg
+                                class="ml-0.5 h-4 w-4 fill-current"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
+                                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
+                            </svg>
+                        </a>
+                    </button>
+                </div>
+            </>
+        );
+    }
+}
